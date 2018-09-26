e9b9378b183595b7b98b87ccfa2727c26e70aeff
Andrei PINTA <andrei.pinta@metrosystems.net>
2018-09-26 16:45:03 +0300

    Handle redirects via react router

--- src/main/java/com/api/jwt/jwtpoc/config/WebServerStaticResourceConfiguration.java
+++ src/main/java/com/api/jwt/jwtpoc/config/WebServerStaticResourceConfiguration.java
@@ -1,17 +1,31 @@
-//package com.api.jwt.jwtpoc.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//
-//@Configuration
-//public  class WebServerStaticResourceConfiguration extends WebMvcConfigurationSupport {
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("*").setViewName("forward:/index.html");
-//    }
-//}
+package com.api.jwt.jwtpoc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public  class WebServerStaticResourceConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/static/**")
+                .addResourceLocations("/resources/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+\\-\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/");
+    }
+}
